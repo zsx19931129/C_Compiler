1a46da6212d30fdd646a92f92369738e666fbf49
Shawn Zhang <zsx19931129@163.com>
2016-12-28 10:41:40 +0800

    Initial Commit

---
+++ C_Compiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_Compiler
+//
+//  Created by 张少雄 on 12/28/16.
+//  Copyright © 2016 张少雄. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
